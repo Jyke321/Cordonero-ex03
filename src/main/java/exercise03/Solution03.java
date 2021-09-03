e73cabf4e34e18a3115fffe41f8049fdaa54c9dc
Jyke321 <jyke321@gmail.com>
2021-09-03 12:59:00 -0400

    Initial commit

---
+++ src/main/java/exercise03/Solution03.java
@@ -0,0 +1,22 @@
+package exercise03;
+
+import java.util.Scanner;
+
+public class Solution03 {
+    public static void main(String[] args) {
+        //prompts the user for a quote and an author
+        //Display the quotation and author
+        //as shown in the example output:
+        /*  What is the quote? These aren't the droids you're looking for.
+            Who said it? Obi-Wan Kenobi
+            Obi-Wan Kenobi says, "These aren't the droids you're looking for."
+        */
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("What is the quote? ");
+        String quote = input.nextLine();
+        System.out.print("Who said it? ");
+        String author = input.nextLine();
+        System.out.println(author + " says, \"" + quote + "\"");
+    }
+}
